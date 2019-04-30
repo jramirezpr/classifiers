d4f87142b453f311913832e3c6f71c594093facb
Juan Carlos Ramirez <the_passenger89@hotmail.com>
2019-04-30 08:03:06 -0700

    StackExchange SVM regularized

---
+++ linearSVM.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 29 17:03:54 2019
+
+@author: Guest
+"""
+
+import pandas as pd
+from  sklearn.svm import  LinearSVC
+df = pd.read_csv("data.csv", names=['x', 'y', 'class'], header=None)
+df['class'] = df['class']
+ax1 = df.plot.scatter(x='x',
+                      y='y',
+                      kind= 'scatter',
+                      ax = ax,
+                      c='class', 
+                      colormap='viridis')
+svc = LinearSVC(fit_intercept=False, C=100)
+model = svc.fit(df[['x','y']],df['class'])
+plt.scatter(df['x'],df['y'])
+plt.plot(xx, yy, 'k-')
+plt.show()
