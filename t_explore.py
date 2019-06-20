7a5487a0109aa25f61430217e7bb3d5250e61b2f
juandevteloro <juan.ramirez@garantiasolar.com>
2019-06-20 15:14:06 -0700

    add whitespaces

--- t_explore.py
+++ t_explore.py
@@ -11,13 +11,13 @@ import matplotlib.pyplot as plt
 
 df_sensor = pd.read_csv(r"C:\Users\user\Documents\task\task_data.csv",
                         index_col="sample index")
-fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(14,4))
+fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(14, 4))
 sensor_names = ["sensor{}".format(i) for i in range(10)]
 counter = 0
 df_sensor["bin1"] = pd.cut(df_sensor["sensor1"], 10)
 
-df_sensor["class_label2"]=df_sensor['class_label']==1
-df_sensor["class_label3"]=df_sensor['class_label']==-1
+df_sensor["class_label2"] = df_sensor['class_label']== 1
+df_sensor["class_label3"] = df_sensor['class_label'] == -1
 df_binned = df_sensor[['bin1',
                        'class_label2',
                        'class_label3']].groupby(['bin1']).sum()
