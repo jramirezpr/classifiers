1a6a7b24d8528b9c9450e67859b9d5f9a633519f
juandevteloro <juan.ramirez@garantiasolar.com>
2019-06-20 14:34:27 -0700

    Update t_explore.py

--- t_explore.py
+++ t_explore.py
@@ -15,10 +15,12 @@ fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(14,4))
 sensor_names = ["sensor{}".format(i) for i in range(10)]
 counter = 0
 df_sensor["bin1"] = pd.cut(df_sensor["sensor1"], 10)
-df_sensor[['bin1','sensor1','class_label']].groupby(['bin1','class_label']).count()
+
 df_sensor["class_label2"]=df_sensor['class_label']==1
 df_sensor["class_label3"]=df_sensor['class_label']==-1
-df_binned = df_sensor[['bin1','class_label2','class_label3']].groupby(['bin1']).sum()
+df_binned = df_sensor[['bin1',
+                       'class_label2',
+                       'class_label3']].groupby(['bin1']).sum()
 
 for axrow in axes:
     for ax in axrow:
