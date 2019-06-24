678d51c8cb931f6caf2a21fb52870ecf9cb652a4
juandevteloro <juan.ramirez@garantiasolar.com>
2019-06-24 09:13:44 -0700

    t_explore train_sensor_classifier and split

--- t_explore.py
+++ t_explore.py
@@ -5,7 +5,6 @@ Created on Thu Jun 20 09:06:03 2019
 @author: user
 """
 from sklearn import neighbors
-import numpy as np
 from sklearn import model_selection
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -42,7 +41,7 @@ def plots_per_sensor(df_sensor):
     fig, axes = plt.subplots(nrows=4, ncols=3, figsize=(14, 8))
     for i in range(4):
         for j in range(3):
-            if(i * 3 + j <= 9):
+            if (i * 3 + j) <= 9:
                 col_name = SENSOR_NAMES[counter]
                 df_binned = relative_bin_val(df_sensor, col_name)
                 df_binned.plot.bar(ax=axes[i, j],
@@ -51,12 +50,37 @@ def plots_per_sensor(df_sensor):
     fig.tight_layout()
 
 
-def train_sensor_classifier(df_sensor, sensor_name):
-    X_train, X_test, y_train, y_test = model_selection.train_test_split(
+def split(df_sensor, sensor_name):
+    return model_selection.train_test_split(
         df_sensor[sensor_name],
         df_sensor['Annual Usage'],
         test_size=0.5,
         random_state=1)
-    kf_4 = model_selection.KFold(n_splits=4, shuffle=True, random_state=1)
 
-    
+
+def train_sensor_classifier(x_train, y_train):
+
+    kf_4 = model_selection.KFold(n_splits=4, shuffle=True, random_state=1)
+    max_score = 0
+    best_k = 1
+    score_list = []
+    for i in range(5, 30):
+        knn = neighbors.KNeighborsClassifier(
+            n_neighbors=i,
+            weights='distance'
+            )
+        score = model_selection.cross_val_score(
+            knn,
+            x_train,
+            y_train,
+            cv=kf_4,
+            scoring='balanced_accuracy_score'
+            ).mean()
+        score_list.append(score)
+        if score > max_score:
+            max_score = score
+            best_k = i
+    return {"scores":score_list,
+            "best score": max_score,
+            "best k":best_k
+            }
